1bed542318ea854ed19b63f628835570cc234d2f
Shiyi Guo <shiyi.guo@nyu.edu>
2012-09-23 09:11:17 -0400

    enable video recording

--- src/att/hackathon/sparqcam/MainActivity.java
+++ src/att/hackathon/sparqcam/MainActivity.java
@@ -1,24 +1,83 @@
 package att.hackathon.sparqcam;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import android.media.CamcorderProfile;
+import android.media.MediaRecorder;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Toast;
+import android.widget.VideoView;
 import android.support.v4.app.NavUtils;
 
 public class MainActivity extends Activity {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	final static int REQUEST_VIDEO_CAPTURED = 1;
+	Uri uriVideo = null;
+	VideoView videoviewPlay;
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		Button buttonRecording = (Button)findViewById(R.id.recording);
+		Button buttonPlayback = (Button)findViewById(R.id.playback);
+		videoviewPlay = (VideoView)findViewById(R.id.videoview);
+	    
+	    buttonRecording.setOnClickListener(new Button.OnClickListener(){
+	    	@Override
+	    	public void onClick(View arg0) {
+	    		// TODO Auto-generated method stub
+	    		Intent intent = new Intent(android.provider.MediaStore.ACTION_VIDEO_CAPTURE);
+	    		startActivityForResult(intent, REQUEST_VIDEO_CAPTURED);
+	    	}});
+	    
+	    buttonPlayback.setOnClickListener(new Button.OnClickListener(){
+	    	@Override
+	    	public void onClick(View arg0) {
+	    		// TODO Auto-generated method stub
+	    		if(uriVideo == null){
+	    			Toast.makeText(MainActivity.this, "No Video", Toast.LENGTH_LONG).show();
+	    		}else{
+	    			Toast.makeText(MainActivity.this, "Playback: " + uriVideo.getPath(),
+	    					Toast.LENGTH_LONG).show();
+	    			videoviewPlay.setVideoURI(uriVideo);
+	    			videoviewPlay.start();
+	    		}
+	    	}});
+	}
 
-    
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		// TODO Auto-generated method stub
+		if(resultCode == RESULT_OK){
+			if(requestCode == REQUEST_VIDEO_CAPTURED){
+				uriVideo = data.getData();
+				Toast.makeText(MainActivity.this, uriVideo.getPath(),Toast.LENGTH_LONG).show();
+			}
+		}else if(resultCode == RESULT_CANCELED){
+			uriVideo = null;
+			Toast.makeText(MainActivity.this,"Cancelled!", Toast.LENGTH_LONG).show();
+		}
+	}
 }
